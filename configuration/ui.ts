9d1c674cc52d67a7c65d49563db40a931b67f9f5
ryogarajan5594 <77201941+ryogarajan5594@users.noreply.github.com>
2025-03-05 03:37:51 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Raj Yogarajan @UNC`;
 export const CLEAR_BUTTON_TEXT: string = `New Prep`;
 export const PAGE_TITLE: string = `HiredDSA`;
-export const PAGE_DESCRIPTION: string = `Chat with ${HIREDDSA}, ${Raj + Yogarajan}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${HIREDDSA}, ${Raj}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
