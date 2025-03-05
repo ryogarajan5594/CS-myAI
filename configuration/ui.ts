03e27af83a4dc973c9c1d1822322e0fb9dc4f5ab
ryogarajan5594 <77201941+ryogarajan5594@users.noreply.github.com>
2025-03-05 03:24:03 -0500

    Update ui.ts
    Customized ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `HiredDSA AI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Raj Yogarajan @UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const CLEAR_BUTTON_TEXT: string = `New Prep`;
+export const PAGE_TITLE: string = `HiredDSA`;
+export const PAGE_DESCRIPTION: string = `Chat with ${HIREDDSA}, ${Raj-Yogarajan}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
