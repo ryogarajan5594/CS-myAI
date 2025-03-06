919a80a3719ffa422b5ccefebb5d7a3898182ae7
ryogarajan5594 <77201941+ryogarajan5594@users.noreply.github.com>
2025-03-06 04:13:20 -0500

    Gave ownership and Description of Algo Ninja AI
    Gave ownership and Description of Algo Ninja AI

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Raj Yogarajan`;
+export const OWNER_DESCRIPTION: string = `Former Coding Sensei and DSA Student`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
