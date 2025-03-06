ed9ce09b564ab6aadf0b1d25e0bccf98a9e8235e
ryogarajan5594 <77201941+ryogarajan5594@users.noreply.github.com>
2025-03-06 04:09:37 -0500

    Fixing Customization for Algo Ninja AI
    Fixing Customization for Algo Ninja AI

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Raj Yogarajan @UNC`;
 export const CLEAR_BUTTON_TEXT: string = `New Ninja Quest`;
 export const PAGE_TITLE: string = `Algo`;
-export const PAGE_DESCRIPTION: string = `Chat with ${Algo}, ${Raj}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
